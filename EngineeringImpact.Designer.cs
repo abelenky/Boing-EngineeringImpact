88a4719590257b387c82c7d9023006af1761f829
abelenky <abelenky@alum.mit.edu>
2015-09-16 09:22:38 -0500

    2015 Contest

---
+++ EngineeringImpact.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace EngineeringImpact
+{
+	partial class EngineeringImpact
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Button = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Button
+			// 
+			this.Button.Location = new System.Drawing.Point(12, 12);
+			this.Button.Name = "Button";
+			this.Button.Size = new System.Drawing.Size(959, 31);
+			this.Button.TabIndex = 0;
+			this.Button.Text = "Engineering Impact";
+			this.Button.UseVisualStyleBackColor = true;
+			this.Button.Click += new System.EventHandler(this.OnClick);
+			// 
+			// EngineeringImpact
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1007, 49);
+			this.Controls.Add(this.Button);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "EngineeringImpact";
+			this.Text = "Cerner Engineering Impact";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Button;
+	}
+}
+
---
+++ EngineeringImpact.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EngineeringImpact
+{
+	public partial class EngineeringImpact : Form
+	{
+/* 1*/	private System.Timers.Timer timer = new System.Timers.Timer(15);
+/* 2*/	private String ImpactText = "Engineering Impact";
+/* 3*/	private int Delta = 1;
+/* 4*/	private int Spacing = 0;
+/* 5*/	private int MaxSpacing = 10;
+
+		public EngineeringImpact()
+		{
+			InitializeComponent();
+/* 6*/		timer.Elapsed += OnTimer;
+/* 7*/		timer.SynchronizingObject = this;
+		}
+
+/* 8*/		private void OnClick(object sender, EventArgs e)
+/* 9*/		{
+/*10*/			if (!timer.Enabled)
+/*11*/			{
+/*12*/				timer.Start();
+/*13*/				new System.Media.SoundPlayer(Properties.Resources.Boing).Play();
+/*14*/			}
+/*15*/		}
+
+/*16*/		private void OnTimer(Object source, System.Timers.ElapsedEventArgs e)
+/*17*/		{
+/*18*/			Button.Text = String.Join(new String(' ', Spacing=Spacing+Delta), ImpactText.ToCharArray().Select(c => c.ToString()).ToArray());
+/*19*/			Delta = (Spacing == MaxSpacing || Spacing == 0) ? -Delta : Delta;
+/*20*/			if (Spacing == 0 && --MaxSpacing == 0)
+/*21*/			{
+/*22*/				if ((source as System.Timers.Timer) != null) (source as System.Timers.Timer).Stop();
+/*23*/
+/*24*/				Delta = 1;
+/*25*/				Spacing = 0;
+/*26*/				MaxSpacing = 10;
+/*27*/			}
+/*28*/		}
+	}
+}
